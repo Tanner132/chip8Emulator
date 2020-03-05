6ce58f109dae5db4b3b7820ae01b071fad64124c
Tanner Strickland <tanner@keysysconsulting.com>
2020-03-05 00:12:21 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chip8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryReader reader = new BinaryReader(new FileStream("IBM Logo.ch8", FileMode.Open)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    var opcode = (ushort)((reader.ReadByte() << 8) | reader.ReadByte());
+                    Console.WriteLine($"{opcode.ToString("X4")}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
